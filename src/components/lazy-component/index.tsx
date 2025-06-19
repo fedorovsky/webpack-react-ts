a67647d86c74a9021f840c54e8f6df0abc13f77e
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-19 21:32:26 +0200

    added lazy component

--- src/components/lazy-component/index.tsx
+++ src/components/lazy-component/index.tsx
@@ -1,13 +1,1 @@
-import * as React from 'react';
-
-const Component = React.lazy(() =>
-  import('./component').then((module) => ({ default: module.Component })),
-);
-
-export const LazyComponent = () => {
-  return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <Component />
-    </React.Suspense>
-  )
-};
+export * from './lazy-component';
---
+++ src/components/lazy-component/lazy-component.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Component = React.lazy(() =>
+  import('./component').then((module) => ({ default: module.Component })),
+);
+
+export const LazyComponent = () => {
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Component />
+    </React.Suspense>
+  );
+};
